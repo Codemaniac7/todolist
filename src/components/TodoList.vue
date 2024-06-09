e4dd4464b4c28ca69bb57def034e8fe766310eaa
Jung Seung Min <jsm1592@gmail.com>
2024-06-09 19:25:11 +0900

    'CompositionAPI_todolist'

---
+++ src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <ul class="list-group">
+        <TodoListItem
+          v-for="todoItem in todoList"
+          :key="todoItem.id"
+          :todoItem="todoItem"
+          @delete-todo="$emit('delete-todo', $event)"
+          @toggle-completed="$emit('toggle-completed', $event)"
+        />
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import TodoListItem from "./TodoListItem.vue";
+
+const props = defineProps({
+  todoList: { type: Array, required: true },
+});
+const emits = defineEmits(["delete-todo", "toggle-completed"]);
+</script>
